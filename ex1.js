e9dc3329aac937d0849edbc83731f9c848968c65
Nare <nararmeni08@gmail.com>
2022-11-07 22:57:41 +0400

    first commit

---
+++ ex1.js
@@ -0,0 +1,11 @@
+
+let num = 6;
+let numberlines = 0;
+let text = " ";
+for(index = 0; index < num; index++){
+    text = text + "+";
+}
+while(numberlines < num){
+    console.log(text);
+    numberlines++;
+}
